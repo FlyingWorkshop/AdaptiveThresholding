7a0994eba5561ce8368536d6dd0340e028888dfd
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2022-07-24 19:26:13 -0700

    Add files via upload

--- colors.py
+++ colors.py
@@ -1,25 +1,25 @@
 import numpy as np
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from PIL import Image
 import matplotlib
 import matplotlib.pyplot as plt
-import cv2
 import random
+
 
 from collections import deque as queue
 
 
-def is_valid(array, explored, point):
-    if point in explored:
+def is_valid(array, seen, point):
+    if point in seen:
         return False
 
-    x, y = point
+    y, x = point
     ymax, xmax = array.shape
-    in_bounds = 0 <= x <= xmax and 0 <= y <= ymax
+    in_bounds = (0 <= x < xmax) and (0 <= y < ymax)
     if not in_bounds:
         return False
 
-    is_white = array[x, y] == 255
+    is_white = array[y, x] == 255
     if not is_white:
         return False
 
@@ -31,26 +31,28 @@ def get_neighbors(center):
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 
-def bfs(array, explored, source):
-    x_coords = []
+def bfs(array, seen, source):
     y_coords = []
-    if not is_valid(array, explored, source):
-        return x_coords, y_coords
+    x_coords = []
+    if not is_valid(array, seen, source):
+        return y_coords, x_coords
     q = queue()
     q.append(source)
+    seen.add(source)
     while q:
         center = q.popleft()
-        explored.add(center)
-        x_coords.append(center[0])
-        y_coords.append(center[1])
-        for neighbor in get_neighbors(center):
-            if is_valid(array, explored, neighbor):
+        y_coords.append(center[0])  # remember numpy indexing is flipped!
+        x_coords.append(center[1])
+        neighbors = get_neighbors(center)
+        for neighbor in neighbors:
+            if is_valid(array, seen, neighbor):
+                seen.add(neighbor)
                 q.append(neighbor)
-    return x_coords, y_coords
+    return y_coords, x_coords
 
 
 def main():
-    img = Image.open("data/gates_of_hell_local_median_r10_gridless.jpg")
+    img = Image.open("data/gates_of_hell_local_median_r20_gridless.jpg")
 
     # preprocessing
     a = np.array(img)
@@ -63,20 +65,24 @@ def main():
 
     # find the contiguous white regions in the picture
     explored = set()
-    for source in zip(*np.where(a == 255)):  # look for white pixels
-        x_coords, y_coords = bfs(a, explored, source)
-        b[x_coords, y_coords] = [181, 46, 36]
-        break
 
-    # scaling
-    dpi = matplotlib.rcParams['figure.dpi']
-    height, width = a.shape
-    figsize = (width / float(dpi), height / float(dpi))
-    plt.figure(figsize=figsize)
-    # TODO: get rid of this line (debuggin)
-    b[:10, :10, :10] = [181, 46, 36]
-    plt.imshow(b)
-    plt.show()
+    for source in tqdm(list(zip(*np.where(a == 255)))):  # look for white pixels
+        x_coords, y_coords = bfs(a, explored, source)
+        b[x_coords, y_coords] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+    # to matplotlib
+    # dpi = matplotlib.rcParams['figure.dpi']
+    # height, width = a.shape
+    # figsize = (width / float(dpi), height / float(dpi))
+    # plt.figure(figsize=figsize)
+    # plt.imshow(b)
+    # plt.show()
+
+    output = Image.fromarray(b[...,::-1].astype('uint8'))
+    output.show()
+    filename = input("Enter a filename if you would like to save this image: ")
+    if filename:
+        output.save(f"outputs/{filename}.jpg")
 
 
 if __name__ == "__main__":
